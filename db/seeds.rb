faeb7e5039c9ce1e1710f930b7c866be66627d40
sidazhang <sidazhang89@gmail.com>
2013-05-15 11:57:38 -0700

    added unique index to email; testing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+sam = User.create(full_name: "samskies", email: "samsamskies@gmail.com")
+sidney = User.create(full_name: "sidney", email: "sidney@gmail.com")
