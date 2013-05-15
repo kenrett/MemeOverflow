d931e86326dfe25e88ff128d98d5a834ec91a2bd
sidazhang <sidazhang89@gmail.com>
2013-05-15 11:42:01 -0700

    Added create votes table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515183918) do
+ActiveRecord::Schema.define(:version => 20130515184115) do
 
   create_table "memes", :force => true do |t|
     t.string   "url",                       :null => false
@@ -34,5 +34,16 @@ ActiveRecord::Schema.define(:version => 20130515183918) do
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
   end
+
+  create_table "votes", :force => true do |t|
+    t.string   "type",       :null => false
+    t.integer  "meme_id"
+    t.integer  "voter_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "votes", ["meme_id"], :name => "index_votes_on_meme_id"
+  add_index "votes", ["voter_id"], :name => "index_votes_on_voter_id"
 
 end
