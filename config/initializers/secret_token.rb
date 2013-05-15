f4ccf72f97f2e2ed15ac28789b8b412d6bb113fc
fmackojc <fab.mackojc@gmail.com>
2013-05-15 11:05:42 -0700

    first commit, new rails app generated with pg database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemeOverflow::Application.config.secret_token = '20ff5274e82f7d38cba8b3a33fec7f17519bfb0a86fd4309e133e9c40280510d9db97e622718c11493b403b9de6a41ac591034ad36bb346cc8e1d7ffd6f020d5'
