2f29a4661adf4f44ff7d6c8397828fd1bf69653c
stephenitis <nguyenhstephen@gmail.com>
2013-05-16 16:27:43 -0700

    page reloads after a user uploads a image

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,12 +14,10 @@ function openFilePicker() {
     },
     function(FPFiles){
       $.each(FPFiles, function(index, FPFile){
-        // var tempID = allTheMemes[index].id + 100;
-        // var meme = new Meme (tempID, FPFile.url);
-        // allTheMemes.push(meme);
-        $.post('memes', FPFile);
-        // var memeDiv = ['<div id="', tempID,'" class="meme">',meme.smallImg,'</div>'].join('');
-        // $('#display-all-memes').prepend(memeDiv);
+        $.post('memes', FPFile).done(function() { 
+          location.reload();
+          alert("Your image has been posted!") 
+        });
       });
     },
     function(FPError){
