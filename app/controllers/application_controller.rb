039cb36723b0a40e923a4a61a5372531791e645f
sidazhang <sidazhang89@gmail.com>
2013-05-15 20:04:05 -0700

    added current user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by_uid(session[:id]) if session[:id]
+  end
 end
