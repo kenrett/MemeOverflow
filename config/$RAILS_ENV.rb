df1f8a8dc0c2625ef496a25ba8242c0a7fc664c4
stephenitis <nguyenhstephen@gmail.com>
2013-05-16 15:04:43 -0700

    setup mailer to send email to user on first signup

---
+++ config/$RAILS_ENV.rb
@@ -0,0 +1,10 @@
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["binaryskipper"],
+    password: ENV["dbcmemes"]
+  }
