14bf9257202e3bff8424ae1fb9f4c6b2d6c91f2f
stephenitis <nguyenhstephen@gmail.com>
2013-05-15 20:02:05 -0700

    added a vote helper for working vote controller

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,9 +1,24 @@
 class VoteController < ApplicationController
   def create
-    p params[:vote_type]
-    p params[:id]
+    @vote = Vote.where(voter_id: session[:id], meme_id: params[:id]).first
+
+    create_or_update_vote(@vote, params, session)
+
+    redirect_to :root
   end
 
+#is this useless now? our create method is doing both a update and create 
   def update
+  end
+
+  private
+  def create_or_update_vote(vote, params, session)
+    if vote
+      vote.update_attributes(vote_type: params[:vote_type])
+      flash[:notice] = "Your vote has been updated!"
+    else
+      Vote.create(vote_type: params[:vote_type], voter_id: session[:id], meme_id: params[:id])
+      flash[:notice] = "Your vote has been created!"
+    end
   end
 end
