ab9ec67ab8dc51bdaa8774f1af77975a4fccdcbe
sidazhang <sidazhang89@gmail.com>
2013-05-17 15:09:48 -0700

    resolved merged conflicts

--- spec/controllers/present_memes_controller_spec.rb
+++ spec/controllers/present_memes_controller_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe PresentMemesController do
 
   describe "GET 'show'" do
-    let!(:meme) {create(:meme)}
+    let!(:meme) { create(:meme) }
+    let!(:meme2) { create(:meme) }
     it "returns http success" do
       get 'show'
       response.should be_success
