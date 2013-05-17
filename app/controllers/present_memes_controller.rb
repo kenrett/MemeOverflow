888da8e1431e7602a15784d00ad60b97ceff9624
sidazhang <sidazhang89@gmail.com>
2013-05-17 12:28:16 -0700

    extracted methods from next and show methods

--- app/controllers/present_memes_controller.rb
+++ app/controllers/present_memes_controller.rb
@@ -4,6 +4,7 @@ class PresentMemesController < ApplicationController
   def show
     order = PresentMeme.last.parse
     next_meme = order.index(1)
+
     @meme = Meme.find(order[next_meme])
     respond_to do |format|
       format.html { render :layout => false}
@@ -12,16 +13,31 @@ class PresentMemesController < ApplicationController
   end
 
   def next
+    retrieve_last_order
+    find_or_create_new_order_and_change_next_meme_index
+
+    @meme = Meme.find(order[next_meme_index])
+    render :json => { :url => @meme.url, :id => @meme.id }
+  end
+
+private
+  
+  def retrieve_last_order
     last_order = PresentMeme.last.parse
-    if last_order.last == params[:id].to_i
-      order = PresentMeme.produce_new_order.parse
+  end
+
+  def find_or_create_new_order_and_change_next_meme_index
+      if last_order.last == params[:id].to_i
+      generate_new_order
       next_meme_index = 0
     else
       order = last_order
       next_meme_index = order.index(params[:id].to_i) + 1
     end
-    @meme = Meme.find(order[next_meme_index])
-    render :json => { :url => @meme.url, :id => @meme.id }
+  end
+
+  def generate_new_order
+    order = PresentMeme.produce_new_order.parse
   end
 
 end
