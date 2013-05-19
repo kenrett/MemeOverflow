e07ee0972d7155f1e15a4c4de6c7e0cf3d6f5c3b
fmackojc <fab.mackojc@gmail.com>
2013-05-18 17:35:53 -0700

    bug for first time vote on a meme fixed

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,8 +4,8 @@ class VotesController < ApplicationController
   def create
     p params[:meme_id]
     if current_user
-      @vote = Vote.where(voter_id: current_user.id, meme_id: params[:meme_id]).first
-        create_or_update_vote(@vote)
+      # @vote = Vote.where(voter_id: current_user.id, meme_id: params[:meme_id]).first
+      @vote = create_or_update_vote
     else
       redirect_to "/auth/google_oauth2"
     end
@@ -13,14 +13,16 @@ class VotesController < ApplicationController
 
   private
 
-  def create_or_update_vote(vote)
+  def create_or_update_vote
+    vote = Vote.where(voter_id: current_user.id, meme_id: params[:meme_id]).first
     if vote
       vote.update_attributes(vote_type: params[:vote_type])
       flash[:notice] = "Your vote has been updated!"
     else
-      Vote.create(vote_type: params[:vote_type], voter_id: current_user.id, meme_id: params[:meme_id])
+      vote = Vote.create(vote_type: params[:vote_type], voter_id: current_user.id, meme_id: params[:meme_id])
       flash[:notice] = "Your vote has been created!"
     end
+    vote
   end
 
 end
