6f2f7b39585bc14eb3ef76d877ccf3ff880ed551
fmackojc <fab.mackojc@gmail.com>
2013-05-18 19:39:56 -0700

    fixed delete button and user show page to display memes correctly 3 per row

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,5 +2,8 @@ class UserController < ApplicationController
   def show
     @user = User.find(params[:id])
     @memes = @user.memes
+    if current_user
+      load_user_votes_hash
+    end
   end
 end
