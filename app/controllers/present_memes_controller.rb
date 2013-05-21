868ee2960df39928741d3b5261e44539a58a433d
sidazhang <sidazhang89@gmail.com>
2013-05-20 22:12:24 -0700

    refactored tv mode controller code

--- app/controllers/present_memes_controller.rb
+++ app/controllers/present_memes_controller.rb
@@ -1,24 +1,33 @@
 class PresentMemesController < ApplicationController
 
   def show
-    if Meme.count == 0
-      flash[:notice] = "There is currently no meme in the database"
-    else
-      current_order = retrieve_current_order
-      @meme = find_next_meme(current_order)
-    end
-    respond_to do |format|
-      format.html { render :layout => false}
-    end
+    Meme.count == 0 ? flash[:notice] = "There is currently no meme in the database" : @meme = find_next_meme(params[:id].to_i)  
+    render :layout => false
   end
 
   def next
-    current_order = retrieve_current_order
-    @meme = find_next_meme(current_order)
+    @meme = find_next_meme(params[:id].to_i)
     render :json => { :url => @meme.url, :id => @meme.id }
   end
 
-private
+  private
+
+  def find_next_meme(id)
+    current_order = retrieve_current_order
+    if !Meme.exists?(id) || last_meme?(current_order, id)
+      order = generate_new_order
+      next_meme_index = 0
+    else
+      order = current_order
+      next_meme_index = order.index(id) + 1
+    end
+    next_meme_id = next_meme_id(order, next_meme_index)
+    Meme.find(next_meme_id)
+  end
+
+  def next_meme_id(order, next_meme_index)
+    order[next_meme_index]
+  end
 
   def retrieve_current_order
     if current_order = PresentMeme.last
@@ -32,23 +41,7 @@ private
     PresentMeme.produce_new_order.parse
   end
 
-  def find_next_meme(current_order)
-    if !Meme.exists?(params[:id].to_i) || last_meme?(current_order)
-      order = generate_new_order
-      next_meme_index = 0
-    else
-      order = current_order
-      next_meme_index = order.index(params[:id].to_i) + 1
-    end
-    next_meme_id = next_meme_id(order, next_meme_index)
-    Meme.find(next_meme_id)
-  end
-
-  def next_meme_id(order, next_meme_index)
-    order[next_meme_index]
-  end
-
-  def last_meme?(current_order)
-    current_order.last == params[:id].to_i
+  def last_meme?(current_order, id)
+    current_order.last == id
   end
 end
