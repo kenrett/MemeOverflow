e910a71b6054acc2e43f2b4eb0296a50d8126d5b
Fab Mackojc <fab.mackojc@gmail.com>
2013-05-24 18:11:56 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Authentication
   protect_from_forgery
+  helper_method :current_user
   helper_method :user_votes
   def user_votes
     @user_votes ||=  current_user.present? ? current_user.meme_votes : {}
