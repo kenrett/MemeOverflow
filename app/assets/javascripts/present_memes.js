925e013419bd269a1c65286ad16c948aa6a73b1b
Fab Mackojc <fab.mackojc@gmail.com>
2013-06-03 15:05:49 -0700

    remove a few console logs from JS code

--- app/assets/javascripts/present_memes.js
+++ app/assets/javascripts/present_memes.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   if ($("div.present-meme-box.pagination-centered").length > 0 ){
    setInterval(function(){
     $.get("/present_memes/next",$("form").serialize()).done(function(data){
-      console.log(data);
       $("div.present-meme-box").children().attr("src",data.url)
       $("form input#id").val(data.id)
     });
